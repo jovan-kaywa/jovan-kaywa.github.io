870bc0e24b27b84a83f44501278e50f971eb9ec3
Jovan Milicevic <jovan@kaywa.com>
2015-08-14 15:45:52 +0200

    Display new field on input field change

--- build/showform.js
+++ build/showform.js
@@ -1,7 +1,29 @@
-var HelloMessage = React.createClass({displayName: "HelloMessage",
-  render: function() {
-    return React.createElement("div", null, "Hello ", this.props.name);
-  }
+var Search = React.createClass({displayName: "Search",
+    getInitialState: function() {
+        return { showResults: false };
+    },
+    onChange: function() {
+        this.setState({ showResults: true });
+    },
+    render: function() {
+        return (
+            React.createElement("div", null, 
+            React.createElement("h3", null, "Display new field on onput field change"), 
+                React.createElement("input", {type: "text", name: "name", placeholder: "First field", onChange: this.onChange}), 
+                 this.state.showResults ? React.createElement(Results, null) : null
+            )
+        );
+    }
 });
 
-React.render(React.createElement(HelloMessage, {name: "John"}), document.getElementById('showForm'));
+var Results = React.createClass({displayName: "Results",
+    render: function() {
+        return (
+            React.createElement("div", {id: "results", className: "search-results"}, 
+                                React.createElement("input", {type: "text", name: "name", placeholder: "Second field"})
+            )
+        );
+    }
+});
+
+React.render(React.createElement(Search, null), document.getElementById('showForm'));
--- src/showform.js
+++ src/showform.js
@@ -1,7 +1,29 @@
-var HelloMessage = React.createClass({
-  render: function() {
-    return <div>Hello {this.props.name}</div>;
-  }
+var Search = React.createClass({
+    getInitialState: function() {
+        return { showResults: false };
+    },
+    onChange: function() {
+        this.setState({ showResults: true });
+    },
+    render: function() {
+        return (
+            <div>
+            <h3>Display new field on onput field change</h3>
+                <input type="text" name="name" placeholder="First field" onChange={this.onChange} />
+                { this.state.showResults ? <Results /> : null }
+            </div>
+        );
+    }
 });
 
-React.render(<HelloMessage name="John" />, document.getElementById('showForm'));
+var Results = React.createClass({
+    render: function() {
+        return (
+            <div id="results" className="search-results">
+                                <input type="text" name="name" placeholder="Second field" />
+            </div>
+        );
+    }
+});
+
+React.render(<Search />, document.getElementById('showForm'));
